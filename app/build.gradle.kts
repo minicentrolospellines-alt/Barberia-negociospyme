167f19ceca62fd0d1c3c54b5b21a8349f6e77829
minicentrolospellines-alt <minicentrolospellines@gmail.com>
2026-09-07 11:50:17 -0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "cl.negociospyme.barberia"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "2.0.0"
+        versionCode = 8
+        versionName = "2.1.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
